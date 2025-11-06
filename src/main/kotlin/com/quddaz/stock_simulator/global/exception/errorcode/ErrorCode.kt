7e4c9dc4e62b01a0c10636bbfd1b01c7632648b9
김병웅 <quddnddl35@naver.com>
2025-11-06 21:36:53 +0900

    feat : ErrorCode 공통화 로직 구현

---
+++ src/main/kotlin/com/quddaz/stock_simulator/global/exception/errorcode/ErrorCode.kt
@@ -0,0 +1,8 @@
+package com.quddaz.stock_simulator.global.exception.errorcode
+
+import org.springframework.http.HttpStatus
+
+interface ErrorCode {
+    val httpStatus: HttpStatus
+    val code: String
+}
