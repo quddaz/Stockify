e0882c59b22487c0f4d20c3e4e6fc03f725f43cd
김병웅 <quddnddl35@naver.com>
2025-11-08 23:25:44 +0900

    feat : OAuth2.0 로그인 성공 핸들러 및 Jwt 검증 필터 도입

---
+++ src/main/kotlin/com/quddaz/stock_simulator/global/security/CustomOAuth2SuccessHandler.kt
@@ -0,0 +1,43 @@
+package com.quddaz.stock_simulator.global.security
+
+import com.quddaz.stock_simulator.domain.oauth.domain.CustomOAuth2User
+import com.quddaz.stock_simulator.domain.oauth.service.JwtTokenProvider
+import com.quddaz.stock_simulator.global.config.jwt.JwtProperties
+import com.quddaz.stock_simulator.global.exception.GlobalException
+import com.quddaz.stock_simulator.global.exception.errorcode.GlobalErrorCode
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.core.Authentication
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler
+import org.springframework.stereotype.Component
+
+@Component
+class CustomOAuth2SuccessHandler(
+    private val jwtTokenProvider: JwtTokenProvider,
+    private val jwtProperties: JwtProperties,
+    @Value("\${oauth2.redirect-uri}") private val redirectUri: String
+) : SimpleUrlAuthenticationSuccessHandler(){
+
+    override fun onAuthenticationSuccess(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authentication: Authentication
+    ) {
+        val customOAuth2User = authentication.principal as CustomOAuth2User
+
+        val accessToken = jwtTokenProvider.createAccessToken(
+            userId = customOAuth2User.id,
+            role = customOAuth2User.role.toString()
+        )
+
+        val refreshTokenCookie = jwtTokenProvider.createRefreshToken(
+            userId = customOAuth2User.id,
+            role = customOAuth2User.role.toString()
+        )
+
+        response.addCookie(refreshTokenCookie)
+        response.setHeader(jwtProperties.header, "${jwtProperties.scheme} $accessToken")
+        response.sendRedirect(redirectUri)
+    }
+}
---
+++ src/main/kotlin/com/quddaz/stock_simulator/global/security/JwtAuthenticationFilter.kt
@@ -0,0 +1,43 @@
+package com.quddaz.stock_simulator.global.security
+
+import com.quddaz.stock_simulator.domain.oauth.service.JwtTokenProvider
+import com.quddaz.stock_simulator.domain.user.entity.User
+import com.quddaz.stock_simulator.global.config.jwt.JwtProperties
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+
+@Component
+class JwtAuthenticationFilter(
+    private val jwtTokenProvider: JwtTokenProvider,
+    private val authenticationMaker: AuthenticationMaker,
+    private val jwtProperties: JwtProperties
+) : OncePerRequestFilter() {
+
+    companion object {
+        private val log = LoggerFactory.getLogger(JwtAuthenticationFilter::class.java)
+
+    }
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        resolveToken(request)?.takeIf { jwtTokenProvider.validateToken(it) }?.let { token ->
+            val user: User = jwtTokenProvider.getUser(token)
+            authenticationMaker.makeAuthentication(user)
+            log.info("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", user.id, request.requestURI)
+        }
+
+        filterChain.doFilter(request, response)
+    }
+
+    private fun resolveToken(request: HttpServletRequest): String? =
+        request.getHeader(jwtProperties.header)
+            ?.takeIf { it.startsWith(jwtProperties.scheme) }
+            ?.removePrefix(jwtProperties.scheme)
+}
