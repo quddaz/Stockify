619e48e39bcb422f7c31c70842beffb6a9bcfefc
김병웅 <quddnddl35@naver.com>
2025-11-11 15:46:49 +0900

    feat : Portfolio 조회 API 작성

---
+++ src/main/kotlin/com/quddaz/stock_simulator/domain/tradeHistory/service/TradeHistoryService.kt
@@ -0,0 +1,16 @@
+package com.quddaz.stock_simulator.domain.tradeHistory.service
+
+import com.quddaz.stock_simulator.domain.tradeHistory.dto.PortfolioResponse
+import com.quddaz.stock_simulator.domain.tradeHistory.repository.TradeHistoryRepository
+import org.springframework.stereotype.Service
+
+@Service
+class TradeHistoryService(
+    private val tradeHistoryRepository: TradeHistoryRepository
+) {
+    /** 포트폴리오 조회 */
+    fun getPortfolioByUser(userId: Long): PortfolioResponse {
+        val portfolio = tradeHistoryRepository.findPortfolioByUser(userId)
+        return PortfolioResponse(portfolio)
+    }
+}
