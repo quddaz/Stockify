fcdd20c88abfb2005221777feec82eaf2e15296a
김병웅 <quddnddl35@naver.com>
2025-11-12 20:40:42 +0900

    feat : 15분 이벤트 주기 스케줄러 구현

--- src/main/kotlin/com/quddaz/stock_simulator/domain/events/service/EventService.kt
+++ src/main/kotlin/com/quddaz/stock_simulator/domain/events/service/EventService.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.quddaz.stock_simulator.domain.events.entity.Event
 import com.quddaz.stock_simulator.domain.events.repository.EventRepository
 import org.springframework.stereotype.Service
+import kotlin.random.Random
 
 @Service
 class EventService(
@@ -15,6 +16,28 @@ class EventService(
         val stream = javaClass.getResourceAsStream(yamlPath) ?: return
         val events = objectMapper.readValue(stream, Array<Event>::class.java).toList()
         eventRepository.saveAll(events)
+    }
+
+
+
+    /** 가중치 기반 무작위 이벤트 선택 */
+    fun getWeightedRandomEvent(): Event {
+        val events = eventRepository.findAll()
+
+        val totalWeight = events.sumOf { it.weight }
+        val rand = (0 until totalWeight).random()  // 0부터 totalWeight-1 사이의 무작위 값 생성
+        var accumulated = 0L
+
+        for (event in events) {
+            accumulated += event.weight
+            if (rand < accumulated) return event
+        }
+        return Event.getDefaultEvent()
+    }
+
+    /** LongRange에서 무작위 Long 값 생성 */
+    private fun LongRange.random(): Long {
+        return start + kotlin.random.Random.nextLong(endInclusive - start + 1)
     }
 
 }
