53b33f1b8b515efe1fb3a9ef6342c96b9195a938
김병웅 <quddnddl35@naver.com>
2025-11-16 20:18:52 +0900

    feat : 인터셉터 로직 변경

--- src/main/kotlin/com/quddaz/stock_simulator/global/security/JwtHandshakeInterceptor.kt
+++ src/main/kotlin/com/quddaz/stock_simulator/global/security/JwtHandshakeInterceptor.kt
@@ -1,9 +1,12 @@
 package com.quddaz.stock_simulator.global.security
 
+import com.quddaz.stock_simulator.domain.oauth.entity.CustomOAuth2User
 import com.quddaz.stock_simulator.domain.oauth.service.JwtTokenProvider
 import org.springframework.http.server.ServerHttpRequest
 import org.springframework.http.server.ServerHttpResponse
 import org.springframework.http.server.ServletServerHttpRequest
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.context.SecurityContextHolderThreadLocalAccessor
 import org.springframework.stereotype.Component
 import org.springframework.web.socket.WebSocketHandler
 import org.springframework.web.socket.server.HandshakeInterceptor
@@ -12,22 +15,13 @@ import org.springframework.web.socket.server.HandshakeInterceptor
 class JwtHandshakeInterceptor(private val jwtProvider: JwtTokenProvider) : HandshakeInterceptor {
 
     override fun beforeHandshake(
-        request: ServerHttpRequest,
-        response: ServerHttpResponse,
-        wsHandler: WebSocketHandler,
-        attributes: MutableMap<String, Any>
+        request: ServerHttpRequest, response: ServerHttpResponse,
+        wsHandler: WebSocketHandler, attributes: MutableMap<String, Any>
     ): Boolean {
-        val token = (request as? ServletServerHttpRequest)?.servletRequest
-            ?.getHeader("Authorization")?.removePrefix("Bearer ")
-        if (token.isNullOrBlank()) return false
+        val auth = SecurityContextHolder.getContext().authentication
+        val customOAuth2User = auth.principal as CustomOAuth2User
 
-        val userId = try {
-            jwtProvider.getUser(token)
-        } catch (e: Exception) {
-            return false
-        }
-
-        attributes["userId"] = userId
+        attributes["userId"] = customOAuth2User.id
         return true
     }
 
